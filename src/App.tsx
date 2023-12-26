5c2775afa70d6c6bc6693d9323680edd0900466f
seungcheol <zjh1687@naver.com>
2023-12-26 23:38:08 +0900

    R3F 기초작업

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { Canvas } from '@react-three/fiber';
 import './App.css';
+import Element3D from './Element3D';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Canvas>
+        <Element3D />
+      </Canvas>
+    </>
+  );
 }
 
 export default App;
